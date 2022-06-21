c76c3ee0214735e0a826e85be60e1412468ab242
shoaibsangi <shoaibsangi218@gmail.com>
2022-06-21 20:45:13 +0500

    All done

--- index.php
+++ index.php
@@ -11,6 +11,24 @@ include 'header.php';
             <div class="slider_bg_box">
                <img src="images/slider-bg.jpg" alt="">
             </div>
+            <div id="customCarousel1" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="container ">
+                        <div class="row">
+                           <div class="col-md-7 col-lg-6 ">
+                              <div class="detail-box">
+                                 <h1>
+                                    <span>
+                                    Sale 20% Off
+                                    </span>
+                                    <br>
+                                    On Everything
+                                 </h1>
+                                 <p>
+                                    Explicabo esse amet tempora quibusdam laudantium, laborum eaque magnam fugiat hic? Esse dicta aliquid error repudiandae earum suscipit fugiat molestias, veniam, vel architecto veritatis delectus repellat modi impedit sequi.
+                                 </p>
+                                 <div class="btn-box">
                                 <div class="btn-box">
                                     <a href="" class="btn1">
                                     Shop Now
